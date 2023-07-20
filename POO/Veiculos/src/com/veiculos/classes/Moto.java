497695115de19917f4fc501e737b7d7e11c09969
Mariana Werneck <marinhomariana8@gmail.com>
2023-07-20 03:52:12 -0300

    :sparkles: feat: Adiciona classe Moto

---
+++ POO/Veiculos/src/com/veiculos/classes/Moto.java
@@ -0,0 +1,11 @@
+package com.veiculos.classes;
+
+public class Moto extends Veiculo {
+    public void abastecer {
+        System.out.println("Método abastecer da classe Moto");
+    };
+
+    public void acelerar {
+        System.out.println("Método acelerar da classe Moto");
+    }
+}
