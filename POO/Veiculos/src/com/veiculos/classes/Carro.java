2bd6144ed81c72eb0b0dab3777cbab54b0d7c2b2
Mariana Werneck <marinhomariana8@gmail.com>
2023-07-20 03:52:04 -0300

    :sparkles: feat: Adiciona classe Carro

---
+++ POO/Veiculos/src/com/veiculos/classes/Carro.java
@@ -0,0 +1,11 @@
+package com.veiculos.classes;
+
+public class Carro extends Veiculo {
+    public void abastecer {
+        System.out.println("Método abastecer da classe Carro");
+    };
+
+    public void acelerar {
+        System.out.println("Método acelerar da classe Carro");
+    }
+}
