78e13d278fa40dabf5f876aa4e767aabdd63f6fd
Mariana Werneck <marinhomariana8@gmail.com>
2023-07-20 06:25:32 -0300

    :boom: fix: Renomeia nome da classe

--- POO/SistemaBancario/src/com/core/Poupanca.java
+++ POO/SistemaBancario/src/com/core/Poupanca.java
@@ -1,6 +1,6 @@
 package com.core;
 
-public class Poupança extends Conta {
+public class Poupanca extends Conta {
     // o método sacar da poupança desconta 2 reais
     @Override
     public void sacar(double valor) {
