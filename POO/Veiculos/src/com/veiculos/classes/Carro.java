f9b71ee1307063b64b23caf109f1b4cbb4fa9525
Mariana Werneck <marinhomariana8@gmail.com>
2023-07-20 04:04:29 -0300

    :boom: fix: Ajuste nos métodos

--- POO/Veiculos/src/com/veiculos/classes/Carro.java
+++ POO/Veiculos/src/com/veiculos/classes/Carro.java
@@ -1,10 +1,6 @@
 package com.veiculos.classes;
 
 public class Carro extends Veiculo {
-    public void abastecer() {
-        System.out.println("Método abastecer da classe Carro");
-    };
-
     public void acelerar() {
         System.out.println("Método acelerar da classe Carro");
     }
--- POO/Veiculos/src/com/veiculos/classes/Moto.java
+++ POO/Veiculos/src/com/veiculos/classes/Moto.java
@@ -1,11 +1,7 @@
 package com.veiculos.classes;
 
 public class Moto extends Veiculo {
-    public void abastecer() {
-        System.out.println("Método abastecer da classe Moto");
-    };
-
     public void acelerar() {
         System.out.println("Método acelerar da classe Moto");
-    };
+    }
 }
