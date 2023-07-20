fdd132585b0cf09682c9475fd04f2028911934b8
Mariana Werneck <marinhomariana8@gmail.com>
2023-07-20 17:40:37 -0300

    :sparkles: feat: Adiciona métodos simularRendimento na classe Poupanca

--- POO/SistemaBancario/src/com/core/Poupanca.java
+++ POO/SistemaBancario/src/com/core/Poupanca.java
@@ -6,4 +6,12 @@ public class Poupanca extends Conta {
     public void sacar(double valor) {
         this.saldo -= valor + 2;
     }
+
+    public void simularRendimento() {
+        saldo += 0.05;
+    }
+
+    public void simularRendimento(double taxa) {
+        saldo += taxa;
+    }
 }
