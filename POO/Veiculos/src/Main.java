1bbc107bed6d44d94fb3790a951bb4ff3235a6f8
Mariana Werneck <marinhomariana8@gmail.com>
2023-07-20 03:56:43 -0300

    :sparkles: feat: Adiciona classe Main

--- POO/Veiculos/src/Main.java
+++ POO/Veiculos/src/Main.java
@@ -1,17 +1,14 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import com.veiculos.classes.Carro;
+import com.veiculos.classes.Moto;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Carro carro = new Carro();
+        carro.abastecer();
+        carro.acelerar();
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        Moto moto = new Moto();
+        moto.abastecer();
+        moto.acelerar();
     }
 }
