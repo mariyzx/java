1dc173300446725094ac3bc4a4949f8d3a508ed6
Mariana Werneck <marinhomariana8@gmail.com>
2023-07-27 15:00:08 -0300

    :sparkles: feat: Adiciona métodos de manipulação de arquivos

--- Manipulação de arquivos/AgendaContatos/src/agendac/Agenda.java
+++ Manipulação de arquivos/AgendaContatos/src/agendac/Agenda.java
@@ -1,23 +1,66 @@
 package agendac;
 
+import java.io.*;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Agenda {
+    private File arquivoBancoDeDados;
     private List<Contato> contatos;
 
     public Agenda() {
-        this.conttos = new ArrayList<Contato>();
+        this.contatos = new ArrayList<Contato>();
+        this.arquivoBancoDeDados = new File("D:\\agenda.txt");
+
     }
 
     public void adicionarContato(Contato contato) {
-        this.contatos.add(contato);
-        System.out.println("Contato adicionado com sucesso");
+        try {
+            FileWriter escritorArquivo = null;
+            BufferedWriter bufferedEscritor = null;
+
+            if (!this.arquivoBancoDeDados.exists()) {
+                arquivoBancoDeDados.createNewFile();
+            }
+
+            escritorArquivo = new FileWriter(arquivoBancoDeDados, true);
+            bufferedEscritor = new BufferedWriter(escritorArquivo);
+
+            bufferedEscritor.write(contato.toString());
+            bufferedEscritor.newLine();
+            bufferedEscritor.flush();
+
+            escritorArquivo.close();
+            bufferedEscritor.close();
+
+            this.contatos.add(contato);
+            System.out.println("Contato adicionado com sucesso!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public void exibirContatos() {
-        for (Contato contato : contatos) {
-            System.out.println("Nome: " + contato.getNome());
-            System.out.println("Telefone: " + contato.getTelefone());
+        try {
+            FileReader leitor = null;
+            BufferedReader bufferedLeitor = null;
+
+            if (arquivoBancoDeDados.exists()) {
+                leitor = new FileReader(arquivoBancoDeDados);
+                bufferedLeitor = new BufferedReader(leitor);
+
+                String linha = bufferedLeitor.readLine();
+
+                while (linha != null) {
+                    System.out.println(linha);
+                    linha = bufferedLeitor.readLine();
+                }
+
+                leitor.close();
+                bufferedLeitor.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
