5fa323b8653890947806db746f1d45d641b0c0a5
Mariana Werneck <marinhomariana8@gmail.com>
2023-08-11 10:10:54 -0300

    feat: Tratando erros com Optional

--- Tratamento de Erros/TryCatch/src/Main.java
+++ Tratamento de Erros/TryCatch/src/Main.java
@@ -1,3 +1,7 @@
+import Pessoa.Pessoa;
+
+import java.util.Optional;
+
 public class Main {
   public static void main(String[] args) {
     try {
@@ -7,6 +11,14 @@ public class Main {
       System.out.println(resultado);
     } catch (ArithmeticException e) {
       System.out.println("Não é possível dividir por 0");
+    }
+
+    Pessoa pessoa = new Pessoa("Mari");
+    Optional<Pessoa> optional = Optional.ofNullable(pessoa); // Verifica se de fato existe algo dentro de pessoa
+    if (optional.isPresent()) {
+      System.out.println("Pessoa existe");
+    } else {
+      System.out.println("Pessoa não existe");
     }
   }
 }
